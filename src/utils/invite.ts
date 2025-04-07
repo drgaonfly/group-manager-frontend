c3dfbd9610aa2ee73a36aef489da4ec4bee02276
efihfaifjw <2218344129@qq.com>
2025-04-07 09:33:10 +0800

    fix: update

--- src/utils/invite.ts
+++ src/utils/invite.ts
@@ -1,14 +1,14 @@
 // 保存邀请码到 sessionStorage
 export const saveInviteCode = (code: string) => {
-  sessionStorage.setItem('inviteCode', code);
+  localStorage.setItem("inviteCode", code);
 };
 
 // 获取邀请码
 export const getInviteCode = () => {
-  return sessionStorage.getItem('inviteCode');
+  return localStorage.getItem("inviteCode");
 };
 
 // 清除邀请码
 export const clearInviteCode = () => {
-  sessionStorage.removeItem('inviteCode');
+  localStorage.removeItem("inviteCode");
 };
