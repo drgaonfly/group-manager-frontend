bb5df35378efa8dc59e8d6f58df09575707fbb86
John Doe <john@example.com>
2025-04-27 15:06:41 +0800

    build: 添加 terser 和 vite-plugin-bundle-obfuscator 依赖
    为了优化代码压缩和混淆，添加了 terser 和 vite-plugin-bundle-obfuscator 依赖，并在 vite 配置中启用了相关功能。

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,58 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import vitePluginBundleObfuscator from "vite-plugin-bundle-obfuscator";
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+  plugins: [
+    react(),
+    vitePluginBundleObfuscator({
+      excludes: [],
+      enable: true,
+      log: true,
+      autoExcludeNodeModules: true,
+      threadPool: true,
+      options: {
+        compact: true,
+        controlFlowFlattening: true,
+        controlFlowFlatteningThreshold: 1,
+        deadCodeInjection: false,
+        debugProtection: false,
+        debugProtectionInterval: 0,
+        disableConsoleOutput: false,
+        identifierNamesGenerator: "hexadecimal",
+        log: false,
+        numbersToExpressions: false,
+        renameGlobals: false,
+        selfDefending: true,
+        simplify: true,
+        splitStrings: false,
+        ignoreImports: true,
+        stringArray: true,
+        stringArrayCallsTransform: true,
+        stringArrayCallsTransformThreshold: 0.5,
+        stringArrayEncoding: [],
+        stringArrayIndexShift: true,
+        stringArrayRotate: true,
+        stringArrayShuffle: true,
+        stringArrayWrappersCount: 1,
+        stringArrayWrappersChainedCalls: true,
+        stringArrayWrappersParametersMaxCount: 2,
+        stringArrayWrappersType: "variable",
+        stringArrayThreshold: 0.75,
+        unicodeEscapeSequence: false,
+      },
+    }),
+  ],
+  build: {
+    minify: "terser", // 使用 terser 来支持更多压缩配置
+    terserOptions: {
+      compress: {
+        drop_console: true, // 移除 console
+        dead_code: true, // 删除未使用代码
+      },
+      format: {
+        comments: false, // 移除注释
+      },
+    },
+  },
+});
