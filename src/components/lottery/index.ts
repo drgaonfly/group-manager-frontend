fc19b51f2c37587f4617b72c7398ca5aff38a4dc
dragonfly <2237676323@qq.com>
2026-09-03 16:22:31 +0800

    feat: clear

--- src/components/lottery/index.ts
+++
@@ -1,4 +0,0 @@
-export { default as LotteryForm } from "./LotteryForm";
-export { default as LotteryHistory } from "./LotteryHistory";
-export { default as ParticipantsTable } from "./ParticipantsTable";
-export * from "./types";
--- src/main.tsx
+++ src/main.tsx
@@ -1,46 +1,19 @@
 import TanstackProvider from "./providers/TanstackProvider";
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-import "./index.css";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import Home from "./routes/Home";
 import Warn from "./routes/Warn";
-import LotteryApp from "./routes/lottery";
-import LotteryCreate from "./routes/lottery/Create";
-import LotteryHistory from "./routes/lottery/history";
-import RedPacketApp from "./routes/redpacket";
-import RedPacketCreate from "./routes/redpacket/Create";
-import RedPacketHistory from "./routes/redpacket/history";
-import TeachingApp from "./routes/teaching";
 import "./i18n";
+import "./style/index.css";
 
 const router = createBrowserRouter([
   {
-    path: "/lottery",
-    element: <LotteryApp />,
-    children: [
-      { path: "create", element: <LotteryCreate /> },
-      { path: "history", element: <LotteryHistory /> },
-    ],
-  },
-  {
-    path: "/redpacket",
-    element: <RedPacketApp />,
-    children: [
-      { path: "create", element: <RedPacketCreate /> },
-      { path: "history", element: <RedPacketHistory /> },
-    ],
-  },
-  {
-    path: "/teaching",
-    element: <TeachingApp />,
-  },
-  {
-    path: "/:query",
+    path: "/",
     element: <Home />,
   },
   {
-    path: "/",
+    path: "/warn",
     element: <Warn />,
   },
 ]);
