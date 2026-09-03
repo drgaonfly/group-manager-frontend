a48d0581d3c463b8511154377b4569c59b0947f7
dragonfly <2237676323@qq.com>
2026-09-03 16:29:45 +0800

    feat: clear 2

--- src/i18n/resources.ts
+++ src/i18n/resources.ts
@@ -1,36 +1,13 @@
 import en from "./en/translation.json";
-import zh from "./zh/translation.json";
-import zhTW from "./zh-TW/translation.json";
-import ja from "./ja/translation.json";
-import ko from "./ko/translation.json";
-import fr from "./fr/translation.json";
-import pt from "./pt/translation.json";
-import de from "./de/translation.json";
+import cn from "./cn/translation.json";
+
 
 const resources = {
   en: {
     translation: en.translation,
   },
-  zh: {
-    translation: zh.translation,
-  },
-  "zh-TW": {
-    translation: zhTW.translation,
-  },
-  ja: {
-    translation: ja.translation,
-  },
-  ko: {
-    translation: ko.translation,
-  },
-  fr: {
-    translation: fr.translation,
-  },
-  pt: {
-    translation: pt.translation,
-  },
-  de: {
-    translation: de.translation,
+  cn: {
+    translation: cn.translation,
   },
 };
 
